0f372622da5c7844f6f8ef7037fe5182ba5f3855
nicholas maxwell <nick@nick-portable.localdomain>
2010-04-25 04:28:31 -0500

    some more work

--- pyTexNotes.py
+++ pyTexNotes.py
@@ -2,70 +2,140 @@
 import sys
 import os
 import time
-
-from settings import *
+from glob import iglob
+import shutil
+import getopt
 
 
 class document:
     n_secions=0
+    input_file=None
     
     
-    
-def parse(file ):
+def parse(input ):
     print "parsing..."
+    
+    
+    
+    
+    for number, line in enumerate(input):
+        if line == "%<<setup>>":
+            n1 = number
+            for k in range(n1,len(input)):
+                if line == "%<</setup>>":
+                
+    
+    
+    
+    
+    
+    
+    
     doc = document()
-    
-    
+    doc.input_file=input
     
     return doc
 
 
-def compile(doc, output_fname, preamble=std_preamble, macros="" ):
+def compile(doc, output, macro, preamble ):
     print "compiling..."
     
     out_file = open(output_fname, 'w')
     
-    out_file.write(preamble)
-    out_file.write(macros)
+    for line in preamble:
+        out_file.write(line)
+    
+    for line in macro:
+        out_file.write(line)
+    
     out_file.write("\n\\begin{document}\n")
     
-    out_file.write("afdlkngrangkrngfkrNG rtorngonrg \n rtreqt aretqret ateqtr4q")
+    for line in doc.input_file:
+        out_file.write(line)
     
     out_file.write("\n\\end{document}\n")
     out_file.close()
-    time.sleep(0.0)
+
+
+
+def parse_args(args, opts):
     
-    os.system( "pdflatex " + output_fname )
-    basename, extension = os.path.splitext(output_fname)
-    os.system( "rm " + basename + '.log' )
-    basename, extension = os.path.splitext(output_fname)
-    os.system( "rm " + basename + '.aux' )
+    values = [ [] for opt in opts ]
+    flat_opts = []
+    for opt in opts:
+        if isinstance(opt,list):    
+            flat_opts.extend(opt)
+        else:
+            flat_opts.append(opt)
+    
+    for arg_number, arg in enumerate(args):
+        for opt_number, opt in enumerate(opts):
+            if arg in opt and len(args) > arg_number+1 and args[arg_number+1] not in flat_opts:
+                values[opt_number].append(args[arg_number+1])
+    
+    return values
 
 
 
 if __name__ == "__main__":
     
-    args = []
-    for arg in sys.argv:
-        if arg != "pyMathNotes.py":
-            args.append(arg)
+    args = sys.argv[1:]
     
-    files = []
-    for arg_number, arg in enumerate(args):
-        try:
-            file = open(arg)
-            out_fname = ""
-            if '.' in arg:
-                basename, extension = os.path.splitext(arg)
-                out_fname = basename + '.tex'
-            else:
-                out_fname = arg + '.tex'
-            files.append( (file, out_fname) )
+    [input_args, output_args, macro_args, preamble_args] = parse_args(args, [['-in','-i','-input','--input'], ['-out','-o','-output','--output'], ['-m','-macros','--macros'],['-p','-pa','-preamble','--preamble'] ] )
+    
+    input = input_fname = None
+    output = output_fname = None
+    macro = macro_fname = None
+    preamble = preamble_fname = None
+    
+    if len(input_args) == 0:
+        print "error: no input files understood."
+        quit()
+    
+    try:
+        if len(output_args) == 0:
+            output_fname = 'out.tex'
+        else:
+            output_fname = output_args[0]
+        output = open( output_fname, 'w' )
+    except:
+        print "error: couldn't open output file: ", output_fname
+    
+    try:
+        if len(macro_args) == 0:
+            macro_fname = 'std_macros.tex'
+        else:
+            macro_fname = macro_args[0]
+        macro = open( macro_fname, 'r' )
+    except:
+        print "error: couldn't open macro file: ", macro_fname
+    
+    try:
+        if len(preamble_args) == 0:
+            preamble_fname = 'std_preamble.tex'
+        else:
+            preamble_fname = preamble_args[0]
+        preamble = open( preamble_fname, 'r' )
+    except:
+        print "error: couldn't open preamble file: ", preamble_fname
+    
+    
+    for input_fname in input_args:
+        
+        try:    
+            input = open( input_fname, 'r' )
         except:
-            print "arguement ", arg_number+1, " not understood."
-    
-    for (file, out_fname) in files:
-        doc = parse(file)
-        compile(doc, out_fname)
-        file.close()
-    
+            print "error: couldn't open input file: ", input_fname
+            quit()
+        
+        
+        doc = parse(input)
+        compile(doc, output, macro, preamble )
+        input.close()
+        
+        time.sleep(0.1)        
+        os.system( "pdflatex " + output_fname )
+        basename, extension = os.path.splitext(output_fname)
+        os.system( "rm " + basename + '.log' )
+        basename, extension = os.path.splitext(output_fname)
+        os.system( "rm " + basename + '.aux' )
